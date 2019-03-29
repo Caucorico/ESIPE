44e1330bf37d6127e6ef0567cf3382105bb16448
CAU GUILLAUME <gcau1@localhost.localdomain>
2019-03-29 13:55:03 +0100

    Modifications du TP09

--- ProgC/TP-09/Exercice-04/main.c
+++ ProgC/TP-09/Exercice-04/main.c
@@ -9,15 +9,41 @@ void print_info_zone( void* adr )
 
 	printf("%lu\n", info_adr[-1] );
 
-	printf("%lu\n", info_adr[-2] );
+	printf("%x\n", info_adr[-2] );
+
+}
+
+
+/* malloc est genereux --" */
+void print_between_zone( void* adr1, void* adr2 )
+{
+	unsigned char i = 0;
+	char* adr1_char_view = adr1;
+	for ( i = 0 ; i < 128 ; i++ )
+	{
+		printf("%x ", *(adr1_char_view+i));
+	}
+
+	putchar('\n');
+	printf("case 0 = %lu\n", *(adr1_char_view+136));
+	printf("case 1 = %lu\n", *(adr1_char_view+144));
 }
 
 int main(void)
 {
-	int* escroc = malloc(1);
+	int i;
+	char* escroc = (char*)malloc(128);
 	int* escroc2 = malloc(1);
+	for ( i = 0 ; i < 136 ; i++ )
+	{
+		escroc[i] = i;
+	}
+	printf("val(escroc) = %x\n", (unsigned int)*escroc);
+	print_between_zone(escroc,escroc2);
+
 	print_info_zone(escroc);
 	print_info_zone(escroc2);
+	free(escroc);
 	free(escroc);
 	free(escroc2);
 	return 0;
