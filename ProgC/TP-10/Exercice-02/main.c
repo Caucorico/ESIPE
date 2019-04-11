8316535d5d171de9f44ed9f2a6dea6383b10409b
CAU GUILLAUME <gcau1@localhost.localdomain>
2019-04-11 10:33:25 +0200

    modifs

--- ProgC/TP-10/Exercice-02/main.c
+++ ProgC/TP-10/Exercice-02/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include <strings.h>
 
 typedef struct cell
 {
@@ -28,9 +30,25 @@ void swap_mem(void* z1, void* z2, size_t size )
 Cell* allocate_cell(char* first, char* last, int age)
 {
   Cell* new_cell = (Cell*)malloc(sizeof(Cell));
+  char* fn = NULL;
+  char* ln = NULL;
+
   if ( new_cell == NULL ) return NULL;
-  new_cell->first_name = first;
-  new_cell->last_name = last;
+
+  if ( first != NULL ) 
+  {
+  	fn = malloc(strlen(first)+1);
+  	fn = strcpy(fn, first);
+  }
+
+  if ( last != NULL ) 
+  {
+  	ln = malloc(strlen(last)+1);
+  	ln = strcpy(ln, last);
+  }
+
+  new_cell->first_name = fn;
+  new_cell->last_name = ln;
   new_cell->age = age;
   new_cell->next = NULL;
 
@@ -38,37 +56,79 @@ Cell* allocate_cell(char* first, char* last, int age)
 }
 
 /* Insert p2 into p1 if possible.
+ * Insert in age order
  * Return 1 if the insertion was done or 0 if not
- * P2 not null
+ * P1 & P2 not null
  */
-int age_order(Cell* p1, Cell* p2)
+/*int age_order(Cell* p1, Cell* p2)
 {
-  if ( p1 == NULL ) return -1; /* What are you trying ?!? */
+  if ( p1 == NULL ) return -1; /* What are you trying ?!? 
   if ( p2 == NULL ) return -1;
-  printf("hey5\n");
   if ( (p1->age < p2->age) && ( (p1->next == NULL) || (p1->next->age > p2->age) ) )
   {
-    printf("hey\n");
     p2->next = p1->next;
     p1->next = p2;
-    printf("hey2\n");
     return 1;
   }    
   else if ( p2->age < p1->age )
   {
-    printf("debug %p %p\n", p1, p2);
     swap_mem(p1, p2, sizeof(Cell));
-    printf("debug2\n");
     p1->next = p2;
     return 1;
   }
     
   return 0;
+}*/
+
+int age_order(Cell* p1, Cell* p2)
+{
+  if ( p1 == NULL ) return -1; /* What are you trying ?!? */
+  if ( p2 == NULL ) return -1;
+
+  if ( p1->age < p2->age )
+    return -1;
+
+  if ( p1->age == p2->age )
+  	return 0;
+
+  if ( p1->age > p2->age )
+  	return 1;
 }
 
+/* Insert p2 into p1 if possible.
+ * Insert in name order
+ * Return 1 if the insertion was done or 0 if not
+ * P1 & P2 not null
+ */
 int name_order(Cell* p1, Cell* p2)
 {
-  return 0;
+	char* full_name_p1;
+	char* full_name_p2;
+	int ret;
+
+	if ( p1 == NULL ) return -1; /* What are you trying ?!? */
+  if ( p2 == NULL ) return -1;
+
+	full_name_p1 = calloc( strlen(p1->first_name) + strlen(p1->last_name) ,sizeof(char));
+	full_name_p2 = calloc( strlen(p2->first_name) + strlen(p2->last_name) ,sizeof(char));
+  full_name_p1 = strcpy(full_name_p1, p1->first_name);
+  full_name_p2 = strcpy(full_name_p2, p2->first_name);
+  full_name_p1 = strcat(full_name_p1, p1->last_name);
+  full_name_p2 = strcat(full_name_p2, p2->last_name);
+
+  if ( strcmp(full_name_p1, full_name_p2) < 0 )
+	  ret = -1;
+
+  if ( strcmp(full_name_p1, full_name_p2) == 0 )
+  	ret = 0;
+
+  if ( strcmp(full_name_p1, full_name_p2) > 0 )
+  	ret = 1;
+
+  free(full_name_p1);
+  free(full_name_p2);
+
+  return ret;
 }
 
 void ordered_insertion(List* l, Cell* new, int order_func(Cell*, Cell*))
@@ -81,17 +141,36 @@ void ordered_insertion(List* l, Cell* new, int order_func(Cell*, Cell*))
     return;
   }
   
-  while( !order_func(buff, new) ) buff = buff->next;
+  /*while( order_func(buff, new) < 1 ) buff = buff->next;*/
+
+  while ( buff != NULL )
+  {
+  	if ( ( order_func(buff, new) <= 0  ) && ( (buff->next == NULL) || (order_func(buff->next, new) >= 0) ) )
+		{
+		  new->next = buff->next;
+		  buff->next = new;
+		  return;
+		}    
+		else if ( order_func(buff, new) == 1  )
+		{
+		  swap_mem(buff, new, sizeof(Cell));
+		  buff->next = new;
+		  return;
+		}
+		else
+		{
+			buff = buff->next;
+		}
+  }
 }
 
 void print_list(List l)
 {
   while( l != NULL )
   {
-    printf("%d,", l->age);
+    printf("%s %s %d \n", l->first_name, l->last_name, l->age);
     l = l->next;
   }
-  putchar('\n');
 }
 
 void free_list(List l)
@@ -115,23 +194,38 @@ void free_list(List l)
 
 int main(void)
 {
+	FILE* f;
   List l = NULL;
   Cell* b;
-  int i;
+  char fn[100], ln[100];
+  int age;
+
+  f = fopen("liste_nom.txt", "r");
+  if ( f == NULL )
+  {
+  	perror("Error open file ");
+  	return -1;
+  }
+
+  bzero(fn, 100*sizeof(char));
+  bzero(ln, 100*sizeof(char));
   
   srand(time(NULL));
 
-  for ( i = 0 ; i < 32 ; i++ )
+  while ( fscanf(f, "%s %s %d", fn, ln, &age ) != EOF )
   {
-    b = allocate_cell(NULL, NULL, rand()%256 );
-    ordered_insertion(&l, b, age_order );
-  }
+    b = allocate_cell(fn, ln, age );
+    ordered_insertion(&l, b, name_order );
 
-  printf("test\n");
+    bzero(fn, 100*sizeof(char));
+  	bzero(ln, 100*sizeof(char));
+  }
 
   print_list(l);
 
   free_list(l);
+
+  fclose(f);
   
   return 0;
 }
