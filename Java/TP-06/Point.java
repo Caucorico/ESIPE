81203006a15687dfafb113e13fd2b7fd391650a1
CAU GUILLAUME <gcau1@localhost.localdomain>
2019-05-06 17:42:07 +0200

    tp-06

---
+++ Java/TP-06/Point.java
@@ -0,0 +1,29 @@
+package fr.umlv.geom;
+
+public class Point {
+	private final int x;
+	private final int y;
+
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + x + ',' + y + ')';
+	}
+	
+	public void translate(int dx, int dy) {
+	  x += dx;
+	  y += dy;
+	}
+}
