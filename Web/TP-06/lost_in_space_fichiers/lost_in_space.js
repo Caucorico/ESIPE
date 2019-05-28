6e5be855ef213f0efd43085f32f4a19aaf2c2953
CAU GUILLAUME <gcau1@localhost.localdomain>
2019-05-28 15:50:19 +0200

    wab

--- Web/TP-06/lost_in_space_fichiers/lost_in_space.js
+++ Web/TP-06/lost_in_space_fichiers/lost_in_space.js
@@ -24,40 +24,121 @@ class Ship {
 	}
 
 	drawShip( ctx ) {
-		ctx.strokeStyle = "#FF0000"; 
+		ctx.strokeStyle = "#FF0000";
+		ctx.beginPath();
 		ctx.moveTo(this.x, this.y);
 		ctx.lineTo(this.x+20, this.y+50);
 		ctx.lineTo(this.x-20, this.y+50);
 		ctx.lineTo(this.x, this.y);
 		ctx.stroke();
 	}
+
+	applyAcceleration() {
+		this.speedX = clamp(this.speedX + this.accelerationX, -5, 5);
+		this.speedY = clamp(this.speedY + this.accelerationY, -5, 5);
+	}
+
+	applySpeed() {
+		this.x += this.speedX;
+		this.y += this.speedY;
+	}
+}
+
+function clamp( a, min, max )
+{
+	if ( a > max ) return max;
+	else if ( a < min ) return min;
+	else return a;
 }
 
 function playerMoveShip( event ) {
 	if ( event.key === "ArrowUp" ) {
-		game.playerShip.y -= 1;
+		game.playerShip.accelerationY += game.playerShip.accelerationY-0.5;
 	}
 	else if ( event.key === "ArrowDown" ) {
-		game.playerShip.y += 1;
+		game.playerShip.accelerationY = game.playerShip.accelerationY+0.5;
 	}
 	else if ( event.key === "ArrowLeft" ) {
-		game.playerShip.x -= 1;
+		game.playerShip.accelerationX = game.playerShip.accelerationX-0.5;
 	}
 	else if ( event.key === "ArrowRight" ) {
-		game.playerShip.x += 1;
+		game.playerShip.accelerationX = game.playerShip.accelerationX+0.5;
+	}
+}
+
+function playerStopMoveShip( event )
+{
+	if ( event.key === "ArrowUp" ) {
+		game.playerShip.accelerationY = 0;
+	}
+	else if ( event.key === "ArrowDown" ) {
+		game.playerShip.accelerationY = 0;
+	}
+	else if ( event.key === "ArrowLeft" ) {
+		game.playerShip.accelerationX = 0;
+	}
+	else if ( event.key === "ArrowRight" ) {
+		game.playerShip.accelerationX = 0;
 	}
 }
 
 window.onload = function() {
 	let canvas = document.getElementById("game_area");
 	let context = canvas.getContext("2d");
+	context.fillStyle = "black";
 	game = new Game( 600, 600 );
 	game.initPlayerShip();
-	window.addEventListener("keydown", playerMoveShip );
+	/*window.addEventListener("keydown", playerMoveShip );
+	window.addEventListener("keyup", playerStopMoveShip );*/
 
+	let map = {}; // You could also use an array
+	onkeydown = onkeyup = function(e){
+	    e = e || event; // to deal with IE
+	    map[e.keyCode] = e.type == 'keydown';
+	    console.log(map);
+
+	    if ( map[38] && map[40] ) /* up + down*/
+	    {
+	    	game.playerShip.accelerationY = 0;
+	    }
+
+	    if ( !map[38] && !map[40] ) /* ~up + ~down*/
+	    {
+	    	game.playerShip.accelerationY = 0;
+	    }
+
+	    if ( map[37] && map[39] ) /* left + right*/
+	    {
+	    	game.playerShip.accelerationX = 0;
+	    }
+
+	    if ( !map[37] && !map[39] ) /* ~left + ~right*/
+	    {
+	    	game.playerShip.accelerationX = 0;
+	    }
+	    
+	    if ( map[38] ) {/* up */
+	    	game.playerShip.accelerationY = game.playerShip.accelerationY-0.5;
+	    }
+	    if ( map[40] ) {/* down */
+	    	game.playerShip.accelerationY = game.playerShip.accelerationY+0.5;
+	    }
+
+	    if ( map[37] ) {/* left */
+	    	game.playerShip.accelerationX = game.playerShip.accelerationX-0.5;
+	    }
+	    if ( map[39] ) {/* right */
+	    	game.playerShip.accelerationX = game.playerShip.accelerationX+0.5;
+	    }
+	}
+
+	context.save();
 	game.playerShip.drawShip( context );
 
 	this.interval = setInterval(() => {
+		context.clearRect(0,0,600,600);
+		game.playerShip.applyAcceleration();
+		game.playerShip.applySpeed();
 		game.playerShip.drawShip( context );
 	}, 20);
 };
