822a9255768b12ed55e8f9dd9bac07df3241d6ab
Caucorico <guillaume.cau77@gmail.com>
2019-06-09 16:23:23 +0200

    First collisions

--- Web/TP-06/lost_in_space_fichiers/lost_in_space.js
+++ Web/TP-06/lost_in_space_fichiers/lost_in_space.js
@@ -11,25 +11,23 @@ class Game {
 	}
 
 	initPlayerShip() {
-		this.playerShip = new Ship( this.width/2, this.height, 0, 0, 0, 0 );
+		this.playerShip = new Ship( this.width/2, this.height, 0, 0, 0, 0, 0 );
 	}
 
 	initAliens() {
 		for ( let i = 0 ; i < 300 ; i++ )
 		{
-			let x;
 			let y;
-			if ( Math.floor(Math.random()*2)%2 == 0 ) {
-				x = 0;
-			} else {
-				x = 600;
-			}
+			let alien;
+
 			y = Math.floor(Math.random()*601);
 
-			console.log(x);
-			console.log(y);
+			if ( Math.floor(Math.random()*2)%2 == 0 ) {
+				alien = new Ship( 0, y, Math.random(), 1, 0, 0, 1);
+			} else {
+				alien = new Ship( 600, y, -Math.random(), 1, 0, 0, 1);
+			}
 
-			let alien = new Ship( x, y, 5, 5, 0, 0);
 			this.aliens.unshift(alien);
 		}
 	}
@@ -44,7 +42,9 @@ class Game {
 	}
 
 	displayBullet( bullet ) {
-		bullet.drawBullet();
+		if ( !bullet.destroy ) {
+			bullet.drawBullet();
+		}
 	}
 
 	displayBullets() {
@@ -52,33 +52,83 @@ class Game {
 	}
 
 	displayAlien( alien ) {
-		let canvas = document.getElementById("game_area");
-		let ctx = canvas.getContext("2d");
-		alien.drawShip(ctx);
+		if ( !alien.destroy )
+		{
+			let canvas = document.getElementById("game_area");
+			let ctx = canvas.getContext("2d");
+			alien.drawShip(ctx);
+		}
 	}
 
 	displayAliens() {
 		this.aliens.map(this.displayAlien);
 	}
+
+	applyAlienSpeed( alien ) {
+		alien.applySpeed();
+	}
+
+	applyAliensSpeed() {
+		this.aliens.map(this.applyAlienSpeed);
+	}
+
+	treatBulletCollision( ship, bullet ) {
+		if ( bullet.y > ship.y && bullet.y < ship.y+10 && bullet.x > ship.x-5 && bullet.x < ship.x+5 )
+		{
+			ship.setDestroy();
+			bullet.setDestroy();
+		}
+	}
+
+	treatShipCollision( ship1, ship2) {
+		if ( ship1.x-5 < ship2.x+5 && ship1.x+5 > ship2.x-5 && ship1.y < ship2.y+10 && ship1.y+10 > ship2.y ) {
+			ship1.setDestroy();
+			ship2.setDestroy();
+		}
+	}
+
+	treatCollisions() {
+		for ( let j = 0 ; j < this.aliens.length ; j++ ) {
+			if ( !this.aliens[j].destroy ){
+				for ( let i = 0 ; i < this.bullets.length ; i++ ) {			
+					if ( !this.bullets[i].destroy ) {
+						this.treatBulletCollision(this.aliens[j], this.bullets[i]);
+					}
+				}
+				this.treatShipCollision(this.playerShip,this.aliens[j]);
+			}
+		}
+	}
 }
 
 class Ship {
-	constructor( x, y, speedX, speedY, accelerationX, accelerationY ) {
+	constructor( x, y, speedX, speedY, accelerationX, accelerationY, type ) {
 		this.x = x;
 		this.y = y;
-		this.speedX = 0;
-		this.speedY = 0;
+		this.speedX = speedX;
+		this.speedY = speedY;
 		this.accelerationX = 0;
 		this.accelerationY = 0;
+		this.type = type; /* Type 0 = player, type 1 = alien*/
+		this.destroy = false;
 	}
 
 	drawShip( ctx ) {
-		ctx.strokeStyle = "#FF0000";
 		ctx.beginPath();
-		ctx.moveTo(this.x, this.y);
-		ctx.lineTo(this.x+5, this.y+10);
-		ctx.lineTo(this.x-5, this.y+10);
-		ctx.lineTo(this.x, this.y);
+		if ( this.type == 0 ) {
+			ctx.strokeStyle = "#FF0000";
+			ctx.moveTo(this.x, this.y);
+			ctx.lineTo(this.x+5, this.y+10);
+			ctx.lineTo(this.x-5, this.y+10);
+			ctx.lineTo(this.x, this.y);
+		} else {
+			ctx.strokeStyle = "#004400";
+			ctx.moveTo(this.x, this.y+10);
+			ctx.lineTo(this.x+5, this.y);
+			ctx.lineTo(this.x-5, this.y);
+			ctx.lineTo(this.x, this.y+10);
+		}
+		
 		ctx.stroke();
 	}
 
@@ -88,8 +138,25 @@ class Ship {
 	}
 
 	applySpeed() {
-		this.x += this.speedX;
-		this.y += this.speedY;
+		if ( this.type == 0 )
+		{
+			this.x = clamp(this.x+this.speedX,10,590);
+			this.y = clamp(this.y+this.speedY,10,590);
+		} else {
+			if ( clamp(this.y+this.speedY,10,590) == 10 || clamp(this.y+this.speedY,10,590) == 590 ) {
+				this.speedY = -this.speedY*1.1;
+			}
+			this.y = clamp(this.y+this.speedY,10,590);
+
+			if ( clamp(this.x+this.speedX,10,590) == 10 || clamp(this.x+this.speedX,10,590) == 590 ) {
+				this.speedX = -this.speedX;
+			}
+			this.x = clamp(this.x+this.speedX,10,590);
+		}
+	}
+
+	setDestroy() {
+		this.destroy = true;
 	}
 }
 
@@ -101,6 +168,7 @@ class Bullet {
 		this.speedY = speedY;
 		this.accelerationX = accelerationX;
 		this.accelerationY = accelerationY;
+		this.destroy = false;
 	}
 
 	applyAcceleration() {
@@ -121,6 +189,10 @@ class Bullet {
 		ctx.arc(this.x, this.y, 2, 0 , 2 * Math.PI);
 		ctx.fillStyle = "white";
 		ctx.fill();
+	}
+
+	setDestroy() {
+		this.destroy = true;
 	}
 }
 
@@ -181,20 +253,23 @@ window.onload = function() {
 	    if ( map[32] ) { /* space */
 	    	let newBullet = new Bullet( game.playerShip.x, game.playerShip.y, 0, clamp(game.playerShip.speedY, game.playerShip.speedY, -0.5), 0, 0 );
 	    	game.bullets.unshift(newBullet);
-	    	console.log(game.bullets);
 	    }
 	}
 
 	context.save();
 	game.playerShip.drawShip( context );
 
-	this.interval = setInterval(() => {
+	let interval = setInterval(() => {
 		context.clearRect(0,0,600,600);
 		game.playerShip.applyAcceleration();
 		game.playerShip.applySpeed();
 		game.playerShip.drawShip( context );
+		game.applyAliensSpeed();
 		game.treatBullets();
+		game.treatCollisions();
 		game.displayBullets();
 		game.displayAliens();
+
+		if ( game.playerShip.destroy ) clearInterval(interval);
 	}, 20);
 };
