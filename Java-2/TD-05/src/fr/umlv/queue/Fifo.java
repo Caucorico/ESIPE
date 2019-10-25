a3e2b95370c16381445b1786e8034d3aabab51fa
Caucorico <guillaume.cau77@gmail.com>
2019-10-25 19:15:28 +0200

    Fin TP 05

--- Java-2/TD-05/src/fr/umlv/queue/Fifo.java
+++ Java-2/TD-05/src/fr/umlv/queue/Fifo.java
@@ -93,7 +93,7 @@ public class Fifo <E> implements Iterable<E>{
     }
 
     public static void main(String[] args) {
-        Fifo<Integer> fifo = new Fifo<>(10);
+        Fifo<Integer> fifo = new Fifo<>(30);
         for ( int i = 0 ; i < 15 ; i++ ) {
             fifo.offer(i);
         }
