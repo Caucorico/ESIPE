dabc3d93bd006f5053e5b689ada06c5176dc1abb
Caucorico <guillaume.cau77@gmail.com>
2019-11-13 00:12:51 +0100

    Sync & PermitSync

--- Concurrency/TD-06/src/exercice02/Sync.java
+++ Concurrency/TD-06/src/exercice02/Sync.java
@@ -1,13 +1,41 @@
 package exercice02;
 
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Supplier;
 
 public class Sync<V> {
+    private boolean inSafe;
+    private final ReentrantLock safeLock = new ReentrantLock(false);
+    private final ReentrantLock inSafeLock = new ReentrantLock(false);
+
     public boolean inSafe() {
-        // TODO
+        inSafeLock.lock();
+        try {
+            return inSafe;
+        } finally {
+            inSafeLock.unlock();
+        }
     }
 
     public V safe(Supplier<? extends V> supplier) throws InterruptedException {
-        return supplier.get();  // TODO
+        safeLock.lock();
+        try {
+            inSafeLock.lock();
+            try {
+                inSafe = true;
+            } finally {
+                inSafeLock.unlock();
+            }
+            return supplier.get();
+        } finally {
+            safeLock.unlock();
+            inSafeLock.lock();
+            try {
+                inSafe = false;
+            } finally {
+                inSafeLock.unlock();
+            }
+        }
+
     }
 }
