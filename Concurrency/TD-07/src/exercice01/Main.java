0e0592a478b3a0a1e18c30d6fab7d984386c41b7
Caucorico <guillaume.cau77@gmail.com>
2019-11-19 11:45:33 +0100

    Add interruption management at ex01

--- Concurrency/TD-07/src/exercice01/Main.java
+++ Concurrency/TD-07/src/exercice01/Main.java
@@ -6,8 +6,12 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    public final static int THREAD_NUMBER = 4;
+
+    private Thread[] threads = new Thread[THREAD_NUMBER];
+
     private void stopThreadById(int id) {
-        
+        threads[id].interrupt();
     }
 
     private void readStdIn() throws IOException {
@@ -17,12 +21,46 @@ public class Main {
             String line;
             while ((line = reader.readLine()) != null) {
                 var threadId = Integer.parseInt(line);
-
+                this.stopThreadById(threadId);
             }
         }
     }
 
-    public static void main(String[] args) {
+    private void addThread(int id, Thread thread) {
+        threads[id] = thread;
+    }
 
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        Runnable runnable = () -> {
+            while ( !Thread.interrupted() ) {
+                try {
+                    Thread.sleep(1000);
+                } catch ( InterruptedException e ) {
+                    Thread.currentThread().interrupt();
+                    continue;
+                }
+                System.out.println("Thread n°" + Thread.currentThread().getName());
+            }
+
+            /* Not thread safe for the display */
+            System.out.println(Thread.currentThread().getName() + " stopped");
+        };
+
+        for ( var i = 0 ; i < THREAD_NUMBER ; i++ ) {
+            var thread = new Thread(runnable);
+            thread.setName(i+"");
+            main.addThread(i, thread);
+            thread.start();
+        }
+
+        while ( true ) {
+            try {
+                main.readStdIn();
+            } catch (IOException e) {
+                continue;
+            }
+        }
     }
 }
