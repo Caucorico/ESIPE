099819a5bf905f4c5424752934265c9e7928da46
Caucorico <guillaume.cau77@gmail.com>
2020-03-04 18:06:36 +0100

    TextLayout

---
+++ GraphicalInterface/TP-00/ClickGame/app/src/main/java/fr/uge/clickgame/ClickGameActivity.kt
@@ -0,0 +1,12 @@
+package fr.uge.clickgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClickGameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_click_game)
+    }
+}
