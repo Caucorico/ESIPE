5ea27798a389ad4e584bc5c13c577cedc5497aaf
Caucorico <guillaume.cau77@gmail.com>
2020-03-11 17:47:37 +0100

    pop-up

--- GraphicalInterface/TP-00/ClickGame/app/src/main/java/fr/uge/clickgame/ClickGameActivity.kt
+++ GraphicalInterface/TP-00/ClickGame/app/src/main/java/fr/uge/clickgame/ClickGameActivity.kt
@@ -2,11 +2,23 @@ package fr.uge.clickgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class ClickGameActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_click_game)
+
+        val button01:Button = findViewById(R.id.button01)
+
+        button01.setOnClickListener{
+            val toast: Toast = Toast.makeText(this, R.string._activity_click_game_quit, Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
+
+
 }
