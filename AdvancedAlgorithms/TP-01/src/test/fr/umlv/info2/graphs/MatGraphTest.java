453afdf48f3666a598eaad4f1396ebf1f4acdce1
Caucorico <guillaume.cau77@gmail.com>
2020-03-27 14:25:40 +0100

    New tests

---
+++ AdvancedAlgorithms/TP-01/src/test/fr/umlv/info2/graphs/MatGraphTest.java
@@ -0,0 +1,45 @@
+package fr.umlv.info2.graphs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatGraphTest {
+
+    
+
+    @org.junit.jupiter.api.Test
+    void numberOfEdges() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void numberOfVertices() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addEdge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isEdge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getWeight() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void edgeIterator() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void forEachEdge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toGraphviz() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void graphIterator() {
+    }
+}
