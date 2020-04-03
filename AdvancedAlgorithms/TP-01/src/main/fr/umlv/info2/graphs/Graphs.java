d4736bbb9bd0821fd96173699e4caccf24e23437
Caucorico <guillaume.cau77@gmail.com>
2020-04-03 21:14:24 +0200

    file created

---
+++ AdvancedAlgorithms/TP-01/src/main/fr/umlv/info2/graphs/Graphs.java
@@ -0,0 +1,17 @@
+package fr.umlv.info2.graphs;
+
+import java.util.List;
+
+public class Graphs {
+    public static List<Integer> DFS(Graph g, int v0) {
+
+        for ( )
+
+        return null;
+    }
+
+    public static List<Integer> BFS(Graph g, int v0) {
+        /* TODO */
+        return null;
+    }
+}
