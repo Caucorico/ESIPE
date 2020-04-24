069336cd0f16a8def7c3b212e67ba85686dfaf84
Caucorico <guillaume.cau77@gmail.com>
2020-04-24 10:42:23 +0200

    Exercice-04

---
+++ NetwokgProgramming/TD-11/src/fr/upem/net/tcp/ThreadData.java
@@ -0,0 +1,49 @@
+package fr.upem.net.tcp;
+
+import java.io.IOException;
+import java.nio.channels.SocketChannel;
+
+public class ThreadData {
+
+    private final Object lockClientConnectedSocketChannel = new Object();
+    private SocketChannel clientConnectedSocketChannel;
+
+    private final Object lockLastActivity = new Object();
+    private long lastActivity;
+
+    public ThreadData setSocketChannel(SocketChannel clientConnectedSocketChannel) {
+        synchronized (lockClientConnectedSocketChannel) {
+            this.clientConnectedSocketChannel = clientConnectedSocketChannel;
+            tick();
+        }
+        return this;
+    }
+
+    public SocketChannel getSocketChannel() {
+        synchronized (lockClientConnectedSocketChannel) {
+            return this.clientConnectedSocketChannel;
+        }
+    }
+
+    public void tick() {
+        synchronized (lockLastActivity) {
+            lastActivity = System.currentTimeMillis();
+        }
+    }
+
+    public void close() throws IOException {
+        synchronized (lockClientConnectedSocketChannel) {
+            if (clientConnectedSocketChannel != null ) {
+                /* TODO : Ask to the teacher if close input first? */
+
+                clientConnectedSocketChannel.close();
+            }
+        }
+    }
+
+    public void closeIfInactive(int timeout) throws IOException {
+        if ( lastActivity + timeout < System.currentTimeMillis() ) {
+            close();
+        }
+    }
+}
